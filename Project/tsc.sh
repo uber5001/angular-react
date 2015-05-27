a6fbcb58bc198cf47427facbd2687751549e8965
uber5001 <uber5001@gmail.com>
2015-05-27 15:31:34 -0700

    add newlines to tsc output

--- Project/tsc.sh
+++ Project/tsc.sh
@@ -1,3 +1,4 @@
 tsd reinstall --config angular/modules/angular2/tsd.json
 tsc -p angular/modules/angular2
 cp -r angular/dist/js/cjs/* node_modules/angular2/
+find node_modules/angular2 -type f -exec sed -i '' -e '$a\' {} \;
