e62f3123cb9dc5343faaf3f923e69525a41fca21
uber5001 <uber5001@gmail.com>
2015-05-28 11:07:06 -0700

    add gulp tasks for build and watchbuild

--- Project/gulpfile.js
+++ Project/gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
 
-gulp.task('default', function() {
-	
+gulp.task('default', ['build', 'watchbuild']);
+
+gulp.task('watchbuild', function() {
+	gulp.watch('src/**/*.ts', ['build']);
 });
+
+gulp.task('build', shell.task([
+	'tsc -p src',
+	"find dist -type f -exec sed -i '' -e '$a\\' {} \\;"
+]));
