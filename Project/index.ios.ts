da8863e69357f473e39ec809afd345574b9f490a
uber5001 <uber5001@gmail.com>
2015-05-28 09:34:28 -0700

    switch main file to typescript

--- Project/index.ios.ts
+++ Project/index.ios.ts
@@ -1,3 +1,6 @@
+/// <reference path="./node_modules/angular2/atscript/typings/node/node.d.ts"/>
+
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -12,50 +15,50 @@ require('reflect-metadata/Reflect.js');
 //var parse5Adapter = require('angular2/src/dom/parse5_adapter.js');
 
 require('angular2/angular2');
-
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
-
 var Project = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+	render: function() {
+		return (
+			React.createElement(
+				React.View,
+				{style: styles.container},
+				React.createElement(
+					React.Text,
+					{style: styles.welcome},
+					"Welcome to React Native!"
+				),
+				React.createElement(
+					React.Text,
+					{style: styles.instructions},
+					"To get started, edit index.ios.ts"
+				),
+				React.createElement(
+					React.Text,
+					{style: styles.instructions},
+					"Press Cmd+R to reload,\n"
+						+ "Cmd+D or shake for dev menu"
+				)
+			)
+		);
+	}
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+var styles = React.StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
 });
 
-AppRegistry.registerComponent('Project', () => Project);
+React.AppRegistry.registerComponent('Project', () => Project);
