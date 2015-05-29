2f1441d964a397b05b2c237e4ce1041e4670b181
uber5001 <uber5001@gmail.com>
2015-05-29 11:52:18 -0700

    remove Project folder

--- Project/tsc.sh
+++
@@ -1,4 +0,0 @@
-tsd reinstall --config angular/modules/angular2/tsd.json
-tsc -p angular/modules/angular2
-cp -r angular/dist/js/cjs/* node_modules/angular2/
-find node_modules/angular2 -type f -exec sed -i '' -e '$a\' {} \;
