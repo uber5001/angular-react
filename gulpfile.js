799b98b7b01118203c64a29b3244253239cffa4b
uber5001 <uber5001@gmail.com>
2015-06-01 15:53:45 -0700

    reorganize building
    The project has been renamed 'dist', and is stored in dist/
    It no longer exists in the repo
    Gulp now initializes the project and copies angular2 over to it. Gulp also inits all needed deps.

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,10 @@ gulp.task('watchbuild', function() {
 
 gulp.task('build', shell.task([
 	'tsc -p src',
-	"find Project -name '*.js' -type f -exec sed -i '' -e '$a\\' {} \\;",
+	'rm -r dist/node_modules/angular2',
+	'mv dist/angular2 dist/node_modules/angular2',
+	'cp src/angular2package.json dist/node_modules/angular2/package.json',
+	"find dist -name '*.js' -type f -exec sed -i '' -e '$a\\' {} \\;",
 ]));
 
 gulp.task('copyAngular', shell.task([
@@ -18,4 +21,20 @@ gulp.task('copyAngular', shell.task([
 
 gulp.task('tsdAngular', shell.task([
 	"tsd reinstall --config angular/modules/angular2/tsd.json"
+]));
+
+gulp.task('initProject', shell.task([
+	"react-native init dist"
+]));
+
+gulp.task('initDeps', shell.task([
+	"cd dist && npm install --save rx",
+	"cd dist && npm install --save traceur",
+	"cd dist && npm install --save reflect-metadata",
+	"cd dist && npm install --save zone.js",
+	"cd dist && npm install --save rtts_assert",
+	"cd dist && npm install --save parse5",
+	"cd dist && npm install --save css",
+	"cd dist && npm install --save url",
+	"cd dist && npm install --save angular2",
 ]));
