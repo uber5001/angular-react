cd9481c7d94bb74d020f21f48c7b9edb54bbcf6a
uber5001 <uber5001@gmail.com>
2015-06-01 15:58:38 -0700

    angular2 successfully bootstraps without a renderer for react native

--- src/index.ios.ts
+++ src/index.ios.ts
@@ -7,11 +7,12 @@
 var React = require('react-native');
 
 //required for angular:
-require('traceur/bin/traceur-runtime.js');
+var parse5Adapter = require('angular2/src/dom/parse5_adapter.js');
+var traceur = require('traceur/bin/traceur-runtime.js');
 require('reflect-metadata/Reflect.js');
-//var parse5Adapter = require('angular2/src/dom/parse5_adapter.js');
 
-import {Component, View, Renderer, bootstrap, bind} from 'angular2/angular2';
+import {Component, View, Renderer, bootstrap, bind, DOCUMENT_TOKEN} from 'angular2/angular2';
+import {DOM} from 'angular2/src/dom/dom_adapter';
 
 @Component({
 	selector: 'hello-world'
@@ -25,17 +26,30 @@ class HelloWorldComponent {
 class ReactNativeRenderer extends Renderer {
 }
 
-bootstrap(HelloWorldComponent, [
-	ReactNativeRenderer,
-	bind(Renderer).toAlias(ReactNativeRenderer)
-]);
+function run() {
 
-var Project = React.createClass({
-	render: function() {
-		return (
-			React.createElement(React.Text, null, "Woooooo!")
-		);
-	}
-});
+	parse5Adapter.Parse5DomAdapter.makeCurrent();
+	
+	var appDoc = DOM.defaultDoc();
 
-React.AppRegistry.registerComponent('Project', () => Project);
+	var helloWorldEl = DOM.createElement('hello-world');
+	DOM.appendChild(appDoc, helloWorldEl);
+
+	bootstrap(HelloWorldComponent, [
+		ReactNativeRenderer,
+		// bind(Renderer).toAlias(ReactNativeRenderer),
+		bind(DOCUMENT_TOKEN).toValue(appDoc)
+	]);
+
+	var ReactApp = React.createClass({
+		render: function() {
+			return (
+				React.createElement(React.Text, null, "Woooooo!")
+			);
+		}
+	});
+
+	React.AppRegistry.registerComponent('dist', () => ReactApp);
+}
+
+run();
